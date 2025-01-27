dc5fa7005180859b5eb5f8213745aa483148fdfb
SamueleFranzese02 <sfranzese02@gmail.com>
2025-01-27 22:41:13 +0100

    feat: added recipe_book and its methods

--- project.c
+++ project.c
@@ -6,10 +6,37 @@
 
 #define COMMAND 16
 #define BUFFER_DIM 100000
+#define HASH_TABLE_DIM 40000
+
+typedef struct recipe_ingredient {
+    char *ingredient;
+    int weight;
+} recipe_ingredient;
+
+typedef struct hash_table_recipes_item {
+    char *key;
+    recipe_ingredient recipe_ingredients[1000];
+    int recipes_count;
+} hash_table_recipes_item;
+
+typedef struct hash_table_recipes {
+    hash_table_recipes_item *recipes_items;
+    int size;
+    int count;
+} hash_table_recipes;
+
+// Recipe book functions
+int hash_function_recipes(hash_table_recipes *table, char *key);
+hash_table_recipes* hash_table_recipes_create();
+hash_table_recipes_item* hash_table_search_recipes(hash_table_recipes *table, char *key);
+void hash_table_insert_recipes(hash_table_recipes *table, char *recipe);
 
 int main(int arc, char const *argv[]) {
     char buffer[BUFFER_DIM], *command, *recipe, *ingredient;
     int camion_frequency, camion_weight, weight, quantity, expire_time;
+    hash_table_recipes *recipe_book;
+
+    recipe_book = hash_table_recipes_create();
 
     if (fgets(buffer, BUFFER_DIM, stdin) != NULL) {
         camion_frequency = atoi(strtok(buffer, " "));
@@ -22,15 +49,7 @@ int main(int arc, char const *argv[]) {
             command = strtok(buffer, " ");
             if (!strcmp(command, "aggiungi_ricetta")) {
                 recipe = strtok(NULL, " ");
-                ingredient = strtok(NULL, " ");
-                weight = atoi(strtok(NULL, " "));
-                printf("%s %s %s %d", command, recipe, ingredient, weight);
-
-                while ((ingredient = strtok(NULL, " ")) != NULL) {
-                    weight = atoi(strtok(NULL, " "));
-                    printf(" %s %d", ingredient, weight);
-                }
-                printf("\n");
+                hash_table_insert_recipes(recipe_book, recipe);
             } else if (!strcmp(command, "rimuovi_ricetta")) {
                 recipe = strtok(NULL, " ");
                 printf("%s %s\n", command, recipe);
@@ -58,4 +77,78 @@ int main(int arc, char const *argv[]) {
     }
 
     return 0;
+}
+
+int hash_function_recipes(hash_table_recipes *table, char *key) {
+    unsigned long hash = 5381;
+    int character;
+
+    while ((character = *key++)) {
+        hash = ((hash << 5) + hash) + character;
+    }
+
+    return hash % table -> size;
+}
+
+hash_table_recipes* hash_table_recipes_create() {
+    hash_table_recipes *table = (hash_table_recipes *) malloc(sizeof(hash_table_recipes));
+    table -> size = HASH_TABLE_DIM;
+    table -> count = 0;
+
+    table -> recipes_items = calloc(table -> size, sizeof(hash_table_recipes_item));
+
+    return table;
+}
+
+hash_table_recipes_item* hash_table_search_recipes(hash_table_recipes *table, char *key) {
+    int hash = hash_function_recipes(table, key);
+
+    while (table -> recipes_items[hash].key != NULL) {
+        if (strcmp(key, table -> recipes_items[hash].key) == 0) {
+            return &table -> recipes_items[hash];
+        } else {
+            hash++;
+            if (hash >= table -> size) {
+                return NULL;
+            }
+        }
+    }
+
+    return NULL;
+}
+
+void hash_table_insert_recipes(hash_table_recipes *table, char *recipe) {
+    int hash, weight, i = 0;
+    hash_table_recipes_item *recipe_item;
+    char *ingredient;
+
+    hash = hash_function_recipes(table, recipe);
+    recipe_item = hash_table_search_recipes(table, recipe);
+
+    if (recipe_item != NULL) {
+        printf("ignorato\n");
+        return;
+    }
+    
+    while (table -> recipes_items[hash].key != NULL) {
+        hash++;
+    }
+
+    table -> recipes_items[hash].key = strdup(recipe);
+    table -> recipes_items[hash].recipes_count = 0;
+    table -> count++;
+
+    ingredient = strtok(NULL, " ");
+    while (ingredient != NULL) {
+        weight = atoi(strtok(NULL, " "));
+
+        table -> recipes_items[hash].recipe_ingredients[i].ingredient = strdup(ingredient);
+        table -> recipes_items[hash].recipe_ingredients[i].weight = weight;
+
+        i++;
+        ingredient = strtok(NULL, " ");
+    }
+
+    printf("aggiunta\n");
+    return;
 }
